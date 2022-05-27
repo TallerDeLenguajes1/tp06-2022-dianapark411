3bb7093ca235fd5d0530c75dfb223ebea90a16cf
Diana Park <dianapark411@gmail.com>
2022-05-27 13:03:03 -0300

    Avances empleados

--- MyApp/Empleado.cs
+++ MyApp/Empleado.cs
@@ -1,22 +1,37 @@
+ public enum cargo{
+    Auxiliar,
+    Administrativo, 
+    Ingeniero,
+    Especialista, 
+    Investigador
+}
 
 public class Empleado{
 
-    public string nombre;
-    public string apellido;
-    public DateTime fechaNacimiento;
-    public char EstadoCivil;
-    public char Genero;
-    public DateTime FechaIngreso;
-    public double sueldo;
+    //private string nombre;
+    //public string Nombre { get => nombre; set => nombre = value; }
+    public string Nombre {get;set;}
 
-    public enum cargo{
+    //public string apellido;
+    public string Apellido {get;set;}
 
-        Auxiliar,
-        Administrativo, 
-        Ingeniero,
-        Especialista, 
-        Investigador
-    }
+    //public DateTime fechaNacimiento;
+    public DateTime FechaNacimiento {get;set;}
+    
+    //public char EstadoCivil;
+    public char EstadoCivil {get;set;}
+
+    //public char Genero;
+    public char Genero {get;set;}
+    
+    //public DateTime FechaIngreso;
+    public DateTime FechaIngreso {get;set;}
+
+    //public double sueldo;
+    public double Sueldo {get;set;}
+
+    public cargo cargo;
+       
     //enum sirve para controlar que la variable no tome otro valor que no sea el declarado (Cantidad de opciones finita). Ademas explica para que sirve
     // en program -> cargo cargoEmpleado = cargo.Administrativo
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,42 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
 
-int opcion = 0;
-Calculadora calc = new Calculadora(0);
+Console.WriteLine("\nIngrese la cantidad de empleados: ");
+int cantEmpleados = Convert.ToInt32(Console.ReadLine());
+Empleado[] emp = new Empleado[cantEmpleados];
 
-do{
-    Console.WriteLine("\nQue operacion desea realizar?");
-    Console.WriteLine("SUMAR-> 1  -  RESTAR-> 2  -  MULTIPLICAR-> 3  -  DIVIDIR-> 4  -  LIMPIAR-> 5  -  SALIR-> 6:  ");
-    opcion = Convert.ToInt32(Console.ReadLine());
+CultureInfo provider = CultureInfo.InvariantCulture;
+string format = "MM/dd/yyyy";
 
-    double num = 0;
+for (int i = 0; i < cantEmpleados; i++){
+    emp[i] = new Empleado();
+    Console.WriteLine($"\nIngrese los datos del empleado {i+1}");
+    Console.WriteLine("Ingrese el nombre: ");
+    emp[i].Nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese el apellido: ");
+    emp[i].Apellido = Console.ReadLine();
+    Console.WriteLine("Ingrese la fecha de nacimiento (formato mm/dd/aaaa): ");
+    emp[i].FechaNacimiento = DateTime.ParseExact(Console.ReadLine(), format, provider);
+    Console.WriteLine("Ingrese el estado civil  (C: casado, S:soltero): ");
+    emp[i].EstadoCivil = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine("Ingrese el genero (F: femenino, M: masculino): ");
+    emp[i].Genero = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine("Ingrese la fecha de ingreso en la empresa (formato mm/dd/aaaa): ");
+    emp[i].FechaIngreso = DateTime.ParseExact(Console.ReadLine(), format, provider);
+    Console.WriteLine("Ingrese el sueldo: ");
+    emp[i].Sueldo = Convert.ToDouble(Console.ReadLine());
 
-    if(opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4){
-        Console.WriteLine("Ingrese un numero: ");
-        num = Convert.ToDouble(Console.ReadLine());
-    }
-    
+    Console.WriteLine("El cargo se genero aleatoriamente ");
 
-    switch (opcion){
+    Random rnd = new Random();
+    int caso = rnd.Next(1,5);
+
+    switch (caso){
         case 1:
-            calc.Sumar(num);
+            emp[i].cargo = cargo.Auxiliar;
             break;
         case 2:
-            calc.Restar(num);
+            emp[i].cargo = cargo.Administrativo;
             break;
         case 3:
-            calc.Multiplicar(num);
+            emp[i].cargo = cargo.Ingeniero;
             break;
         case 4:
-            calc.Dividir(num);
+            emp[i].cargo = cargo.Especialista;
             break;
         case 5:
-            calc.Limpiar();
+            emp[i].cargo = cargo.Investigador;
             break;
         default:
             break;
     }
+}
 
-    Console.WriteLine("RESULTADO DE LA OPERACION: " + calc.resultado);
+DateTime fechaActual = DateTime.Today;
 
 
-} while (opcion != 6);
+for (int i = 0; i < cantEmpleados; i++){
+    
+    int edad = 0, falta = 0;;
+
+    Console.WriteLine($"\n-----Datos del empleado {i+1}-----");
+    Console.WriteLine($"Nombre: {emp[i].Nombre}");
+    Console.WriteLine($"Apellido: {emp[i].Apellido}");
+    Console.WriteLine($"Fecha de nacimiento: {emp[i].FechaNacimiento.ToShortDateString()}"); 
+    //.ToShortDateString() para que solo muestre la fecha y no la hora que se agrega por defecto a medianoche
+    Console.WriteLine($"Genero (F: femenino, M: masculino): {emp[i].Genero}");
+    Console.WriteLine($"Estado civil (C: casado, S:soltero): {emp[i].EstadoCivil}");
+    Console.WriteLine($"Fecha de ingreso en la empresa: {emp[i].FechaIngreso.ToShortDateString()}");
+    Console.WriteLine($"Sueldo: {emp[i].Sueldo}");
+    Console.WriteLine($"Cargo: {emp[i].cargo}");
+    
+    edad = fechaActual.Year - emp[i].FechaNacimiento.Year;
+    if(emp[i].FechaNacimiento.Month > fechaActual.Month){
+        edad = edad-1;
+    }
+    Console.WriteLine($"Edad: {edad}");
+
+    int antiguedad = fechaActual.Year - emp[i].FechaIngreso.Year;
+    if(emp[i].FechaIngreso.Month > fechaActual.Month){
+        antiguedad = antiguedad-1;
+    }
+    Console.WriteLine($"Antiguedad: {antiguedad}");
+
+    if(Char.ToLower(emp[i].Genero) == 'f'){     //convierto a minuscula para que no haya errores si el usuario ingreso con mayusculas
+        falta = 60 - edad;  //las mujeres se jubilan a los 60 años
+    }else{
+        falta = 65 - edad;  //los varones se jubilan a los 65 años
+    }
+    Console.WriteLine($"Años que faltan para jubilarse: {falta}");
+}
